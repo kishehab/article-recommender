9035fa3769fff86542b46ba026366440079a2c24
Khalil Shehab <khalil.shehab@student.ie.edu>
2024-09-11 13:40:55 +0300

    setup flask with index page

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-## hello
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    user = 'Alice'
+    is_logged_in = True
+    return render_template('index.html', user=user, is_logged_in=is_logged_in)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
