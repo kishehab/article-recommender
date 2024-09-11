a8b265491bc6e13ab024c27bacd0f6a45aac79d5
Khalil Shehab <khalil.shehab@student.ie.edu>
2024-09-11 15:28:11 +0300

    include JS in the static folder and use bootstrap

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,54 @@
 console.log("Main JS is running...");
+window.onload = function() {
+    console.log("Page has fully loaded!");
+    // Function to add a new user
+document.getElementById('userForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const city = document.getElementById('city').value;
+    
+    // Send user data to Flask API
+    const response = await fetch('/add_user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            age: age,
+            city: city
+        })
+    });
+    
+    const result = await response.json();
+    alert(result.message);
+    
+    // Refresh the user list
+    listUsers();
+});
+
+// Function to list all users
+async function listUsers() {
+    const response = await fetch('/list_users', {
+        method: 'GET'
+    });
+    const users = await response.json();
+    
+    // Clear the current list
+    const userList = document.getElementById('userList');
+    userList.innerHTML = '';
+    
+    // Populate the user list
+    users.forEach(user => {
+        const li = document.createElement('li');
+        li.textContent = `Name: ${user.name}, Age: ${user.age}, City: ${user.city}`;
+        userList.appendChild(li);
+    });
+}
+
+// List users when the page loads
+listUsers();
+};
+
